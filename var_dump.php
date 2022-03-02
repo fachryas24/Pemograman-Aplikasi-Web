9c46062364eb2200505edd88e3a33eda02fdc37c
Fachry Arsyad Salam <99635670+fachryas24@users.noreply.github.com>
2022-03-02 23:46:23 +0700

    Add files via upload

---
+++ var_dump.php
@@ -0,0 +1,12 @@
+<?php
+//fungsi var_dump digunakan untuk menampilkan informasi dari variabel
+
+var_dump(1 + 1);
+echo"<br>";
+var_dump("1 + 1");
+echo"<br>";
+var_dump(1 - "1");
+echo"<br>";
+var_dump("1" - "1");
+
+?>
