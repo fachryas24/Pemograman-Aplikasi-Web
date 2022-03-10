6b2b3f979ec5e45568eb19d100ad801c8116792a
Fachry Arsyad Salam <99635670+fachryas24@users.noreply.github.com>
2022-03-10 07:39:51 +0700

    Update var_dump.php

--- var_dump.php
+++ var_dump.php
@@ -1,13 +1,8 @@
 <?php
 
 var_dump(1 + 1);
-echo"<br>";
 var_dump("1 + 1");
-echo"<br>";
 var_dump(1 - "1");
-echo"<br>";
 var_dump("1" - "1");
-
 //fungsi var_dump digunakan untuk menampilkan informasi dari variabel
-
 ?>
